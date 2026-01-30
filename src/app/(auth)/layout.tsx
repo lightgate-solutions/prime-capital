d0509ba9d834381a5800f2450249617dc1fc8e31
Elameen Daiyabu <elameendaiyabu@proton.me>
2026-01-30 22:58:50 +0100

    update

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,7 @@
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import { auth } from "@/lib/auth";
-
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-  if (session) redirect("/");
-
   return <div>{children}</div>;
 }
